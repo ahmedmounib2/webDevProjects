fd5b164e1be1e7045965a2d5170a35611cd2321d
ahmedmounib2 <ahmedmounib2@gmail.com>
2025-02-24 12:26:53 +0200

    Add 299 mapping components practice

---
+++ 299 mapping components practice/src/emojipedia.js
@@ -0,0 +1,32 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "🥸",
+    name: "Disguised face smiley",
+    meaning:
+      "This emoji is used to indicate that someone wants to hide their identity or intentions. It can also express a desire to get to the bottom of something without being recognized. Additionally, it is a symbol of carnival, where people dress up and take on different roles. The characteristics of this disguised face come from the “Groucho mask“ or “Groucho glasses“, designed in reference to the caricature of Julius Henry Marx in the Marx Brothers films. These humorous films were very popular in the 1930s and 1940s, and the mask has been a staple of novelty stores since the 1940s.",
+  },
+];
+
+export default emojipedia;
