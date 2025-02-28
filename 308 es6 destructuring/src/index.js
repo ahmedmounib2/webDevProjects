b56a781e6ec8e0d6bb47e5bd95e7c88927e6f96b
ahmedmounib2 <ahmedmounib2@gmail.com>
2025-02-28 19:20:54 +0200

    Add 308 es6 destructuring

---
+++ 308 es6 destructuring/src/index.js
@@ -0,0 +1,74 @@
+/* import animals, { useAnimals } from "./data";
+
+//console.log(animals);
+// destructuring an array
+const [cat, d] = animals;
+//console.log(c);
+
+//console.log(useAnimals(cat));
+const [animal, makeSound] = useAnimals(cat);
+//console.log(animal);
+makeSound(); */
+
+// destructuring an object, the name has to match the object's properties names while destructuring
+/* const { name, sound } = cat;
+console.log(sound); */
+// giving a different name to these variables when destructuirng an object ex:
+/* const { name: catName, sound: catSound } = cat;
+console.log(catName); */
+
+// giving a default value to our properties
+// if the object didnt have a name property then it will be set to name= fluffy, if it did however it will be updated to fluffy
+/* const { name = "Fluffy", sound = "purr" } = cat;
+console.log(name); */
+
+/* // destructuring a nested object through extensive destructuring
+const {
+  name,
+  sound,
+  feedingRequirements: { food, water },
+} = cat;
+//console.log(feedingRequirements.food);
+
+console.log(food); */
+
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
+
+// console.log(cars);
+const [honda, tesla] = cars;
+
+const {
+  model: hondaModel,
+  coloursByPopularity: [hondaTopColor, hondaSecondColor],
+  speedStats: { topSpeed: hondaTopSpeed, zeroToSixty: hondaZeroToSixty },
+} = honda;
+
+const {
+  model: teslaModel,
+  coloursByPopularity: [teslaTopColor, teslaSecondColor],
+  speedStats: { topSpeed: teslaTopSpeed, zeroToSixty: teslaZeroToSixty },
+} = tesla;
+
+console.log(hondaZeroToSixty);
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{teslaModel}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColor}</td>
+    </tr>
+    <tr>
+      <td>{hondaModel}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColor}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
