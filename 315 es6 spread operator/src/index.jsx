207a7d3c8ee583ea7dfcbfdcd9f2374c41f0b9fd
ahmedmounib2 <ahmedmounib2@gmail.com>
2025-03-03 14:11:23 +0200

    Add 315 es6 spread operator

---
+++ 315 es6 spread operator/src/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// const citrus = ["Lime", "Lemon", "Orange"];
+// const fruits = ["Apple", "Banana", ...citrus];
+// console.log(fruits); // ["Apple", "Banana", "Lime", "Lemon", "Orange"]
+
+// const fullName = {
+//   firstName: "John",
+//   lastName: "Doe"
+// };
+// const user = {
+//   ...fullName,
+//   id: 1,
+//   username: "johndoe"
+// };
+// console.log(user); // {firstName: "John", lastName: "Doe", id: 1, username: "johndoe"}
