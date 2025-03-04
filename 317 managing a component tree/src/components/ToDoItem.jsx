c4ebf5bd240f0e651add0e13818ce0e68a843ef3
ahmedmounib2 <ahmedmounib2@gmail.com>
2025-03-04 17:23:24 +0200

    change the state of a parent componenet to delete from the items array

--- 317 managing a component tree/src/components/ToDoItem.jsx
+++ 317 managing a component tree/src/components/ToDoItem.jsx
@@ -1,21 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
 function ToDoItem(props) {
-  const [isDone, setIsDone] = useState(false);
-
-  function toggleDone() {
-    setIsDone((prevValue) => !prevValue);
-  }
   return (
-    <div onClick={toggleDone}>
-      <li
-        style={{
-          textDecoration: isDone ? "line-through" : "none",
-          cursor: "pointer",
-        }}
-      >
-        {props.text}
-      </li>
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
     </div>
   );
 }
